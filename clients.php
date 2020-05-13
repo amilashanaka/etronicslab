5a3d441ee60b70f568a63141d48d55e4cf664597
dineshrajapakshe <43881151+dineshrajapakshe@users.noreply.github.com>
2020-05-13 21:48:51 +0530

    update

---
+++ clients.php
@@ -0,0 +1,184 @@
+<?php
+include_once './inc/header.php';
+include_once './inc/slider.php';
+?>
+<!-- /.offcanvas-info -->
+<div class="wrapper light-wrapper page-title-wrapper">
+    <div class="container inner text-center">
+        <h1 class="page-title">Grid Portfolio with Details</h1>
+        <p class="lead">Aenean lacinia bibendum nulla sed consectetur</p>
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /.wrapper -->
+<div class="wrapper light-wrapper">
+    <div class="container inner">
+        <div class="grid grid-view boxed">
+            <div class="isotope-filter text-center">
+                <ul>
+                    <li><a class="button active" data-filter="*">All</a></li>
+                    <li><a class="button" data-filter=".branding">Branding</a></li>
+                    <li><a class="button" data-filter=".corporate">Corporate</a></li>
+                    <li><a class="button" data-filter=".application">Application</a></li>
+                    <li><a class="button" data-filter=".video">Video</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+            <div class="tiles text-center light-gallery-wrapper">
+                <div class="row isotope">
+                    <div class="item grid-sizer col-md-6 col-lg-4 corporate application">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p1-full.jpg" class="lightbox"> <img src="style/images/art/p1.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post.html">Fringilla Nullam</a></h2>
+                                <div class="meta mb-0">Corporate, Application</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 application">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p2-full.jpg" class="lightbox"> <img src="style/images/art/p2.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post2.html">Ridiculus Parturient</a></h2>
+                                <div class="meta mb-0">Application</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 corporate branding">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p3-full.jpg" class="lightbox"> <img src="style/images/art/p3.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post3.html">Ornare Ipsum</a></h2>
+                                <div class="meta mb-0">Corporate, Branding</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 application">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p4-full.jpg" class="lightbox"> <img src="style/images/art/p4.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post4.html">Nullam Mollis</a></h2>
+                                <div class="meta mb-0">Application</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 video application">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="https://vimeo.com/90355541" class="lightbox"> <img src="style/images/art/p5.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post5.html">Euismod Risus</a></h2>
+                                <div class="meta mb-0">Video, Application</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 branding">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p6-full.jpg" class="lightbox"> <img src="style/images/art/p6.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post6.html">Ridiculus Tristique</a></h2>
+                                <div class="meta mb-0">Branding</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 branding corporate">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p7-full.jpg" class="lightbox"> <img src="style/images/art/p7.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post.html">Sollicitudin Pharetra</a></h2>
+                                <div class="meta mb-0">Branding, Corporate</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 application video">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="https://www.youtube.com/watch?v=26TbMXXOe0I" class="lightbox"> <img src="style/images/art/p8.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post2.html">Tristique Venenatis</a></h2>
+                                <div class="meta mb-0">Video, Application</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 application">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p9-full.jpg" class="lightbox"> <img src="style/images/art/p9.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post3.html">Cursus Fusce</a></h2>
+                                <div class="meta mb-0">Application</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 branding">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p10-full.jpg" class="lightbox"> <img src="style/images/art/p10.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post4.html">Consectetur Malesuada</a></h2>
+                                <div class="meta mb-0">Branding</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 corporate">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p11-full.jpg" class="lightbox"> <img src="style/images/art/p11.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post5.html">Ultricies Aenean</a></h2>
+                                <div class="meta mb-0">Corporate</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                    <div class="item grid-sizer col-md-6 col-lg-4 branding">
+                        <div class="box bg-white shadow p-30">
+                            <figure class="main overlay overlay2 rounded mb-30"><a href="style/images/art/p12-full.jpg" class="lightbox"> <img src="style/images/art/p12.jpg" alt="" /></a></figure>
+                            <div class="post-content">
+                                <h2 class="post-title mb-10"><a href="portfolio-post6.html">Pellentesque Commodo</a></h2>
+                                <div class="meta mb-0">Branding</div>
+                            </div>
+                            <!-- /.post-content -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.item -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.tiles -->
+        </div>
+        <!-- /.grid -->
+    </div>
+    <!-- /.container -->
+</div>
+<?php
+include_once './inc/footer.php';
+?>
---
+++ contact.php
@@ -0,0 +1,122 @@
+<?php
+include_once './inc/header.php';
+include_once './inc/slider.php';
+?>
+<!-- /.offcanvas-info -->
+<div class="wrapper image-wrapper bg-image page-title-wrapper inverse-text">
+    <div class="container inner text-center">
+        <div class="space90"></div>
+        <h1 class="page-title">Contact Us</h1>
+        <p class="lead">Aenean lacinia bibendum nulla sed consectetur</p>
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /.wrapper -->
+<div class="wrapper gray-wrapper">
+    <div class="container inner">
+        <div class="row text-center gutter-60">
+            <div class="col-md-4">
+                <div class="icon icon-svg mb-20"><img src="style/images/icons/ui-gps.png" alt="" /></div>
+                <p class="mb-0">Moon Street Light Avenue<br> 14/05 Jupiter, JP 80630</p>
+            </div>
+            <!--/column -->
+            <div class="col-md-4">
+                <div class="icon icon-svg mb-20"><img src="style/images/icons/co-telephone.png" alt="" /></div>
+                <p class="mb-0">00 (123) 456 78 90<br> 00 (987) 654 32 10 </p>
+            </div>
+            <!--/column -->
+            <div class="col-md-4">
+                <div class="icon icon-svg mb-20"><img src="style/images/icons/co-email-5.png" alt="" /></div>
+                <p class="mb-0"><a class="nocolor" href="mailto:#">manager@email.com</a><br>
+                    <a class="nocolor" href="mailto:#">asistant@email.com</a></p>
+            </div>
+            <!--/column -->
+        </div>
+        <!--/.row -->
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /.wrapper -->
+<div class="wrapper light-wrapper">
+    <div class="image-block-wrapper">
+        <div class="image-block col-lg-6 p-0">
+            <div class="google-map map-full">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.4564967931838!2d-122.03250858386237!3d37.33169997984258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb5b6e94ffe23%3A0x63bb79532baa6af4!2sApple+Campus!5e0!3m2!1sen!2str!4v1553850783971!5m2!1sen!2str" style="width:100%; height: 100%; border:0" allowfullscreen></iframe>
+            </div>
+            <!--/.google-map -->
+        </div>
+        <!--/.image-block -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-6">
+                    <div class="box d-flex pr-25">
+                        <div class="align-self-center">
+                            <h3>Drop Us a Line</h3>
+                            <p>Fusce dapibus tellus commodo tortor mauris condimentum utellus porta fermentum sem malesuada magna.</p>
+                            <div class="space30"></div>
+                            <form id="contact-form" method="post" action="contact/contact.php">
+                                <div class="messages"></div>
+                                <div class="controls">
+                                    <div class="form-row">
+                                        <div class="col-lg-12 col-xl-6">
+                                            <div class="form-group">
+                                                <input id="form_name" type="text" name="name" class="form-control" placeholder="First Name *" required="required" data-error="First Name is required.">
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-xl-6">
+                                            <div class="form-group">
+                                                <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Last Name *" required="required" data-error="Last Name is required.">
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col-lg-12 col-xl-6">
+                                            <div class="form-group">
+                                                <input id="form_email" type="email" name="email" class="form-control" placeholder="Email *" required="required" data-error="Valid email is required.">
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-xl-6">
+                                            <div class="form-group">
+                                                <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Phone">
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <textarea id="form_message" name="message" class="form-control" placeholder="Message *" rows="4" required="required" data-error="Message is required."></textarea>
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <input type="submit" class="btn btn-send" value="Send message">
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col-md-12">
+                                            <p class="text-muted"><strong>*</strong> These fields are required.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <!-- /form -->
+                        </div>
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!--/column -->
+            </div>
+            <!--/.row -->
+        </div>
+        <!--/.container-fluid -->
+    </div>
+    <!--/.image-block-wrapper -->
+</div>
+<!-- /.wrapper -->
+<?php
+include_once './inc/footer.php';
+?>
